9e535e5fb79124ae3213661b8365f01273cf1fa3
林聖泰 <shengtai0201@gmail.com>
2018-11-07 17:45:38 +0800

    tests

---
+++ SymmetricDS.Admin.Tests/ConfigurationTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricDS.Admin.Tests
+{
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        public ConfigurationTests()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+
+        }
+
+        [Test]
+        public void AAA()
+        {
+
+        }
+    }
+}

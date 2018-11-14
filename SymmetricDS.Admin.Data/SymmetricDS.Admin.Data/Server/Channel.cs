82418f3b870e89afc3fa82825070491c5f58611c
YiTai_Wang <zx096045023@gmail.com>
2018-11-14 10:42:50 +0800

    .

---
+++ SymmetricDS.Admin.Data/SymmetricDS.Admin.Data/Server/Channel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SymmetricDS.Admin.Data.Server
+{
+    public partial class Channel
+    {
+        public Channel()
+        {
+            Trigger = new HashSet<Trigger>();
+        }
+
+        public int Id { get; set; }
+        public string ChannelId { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Trigger> Trigger { get; set; }
+    }
+}
---
+++ SymmetricDS.Admin.Data/SymmetricDS.Admin.Data/Server/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SymmetricDS.Admin.Data.Server
+{
+    public partial class Node
+    {
+        public int Id { get; set; }
+        public int NodeGroupId { get; set; }
+        public int? DatabaseType { get; set; }
+        public string DatabaseHost { get; set; }
+        public string DatabaseName { get; set; }
+        public string DatabaseUser { get; set; }
+        public string DatabasePassword { get; set; }
+        public string SyncUrlPort { get; set; }
+        public string ExternalId { get; set; }
+        public int? JobPurgePeriodTimeMs { get; set; }
+        public int? JobRoutingPeriodTimeMs { get; set; }
+        public int? JobPushPeriodTimeMs { get; set; }
+        public int? JobPullPeriodTimeMs { get; set; }
+        public bool InitialLoadCreateFirst { get; set; }
+        public string NodePassword { get; set; }
+        public int Version { get; set; }
+
+        public NodeGroup NodeGroup { get; set; }
+    }
+}

9ed6f791ce3e161f45b74402817deb48fb97280e
YiTai_Wang <zx096045023@gmail.com>
2018-11-15 11:01:30 +0800

    ...

--- SymmetricDS.Admin.Data/Server/Router.cs
+++ SymmetricDS.Admin.Data/Server/Router.cs
@@ -13,8 +13,8 @@ namespace SymmetricDS.Admin.Server
         public int Id { get; set; }
         public int ProjectId { get; set; }
         public string RouterId { get; set; }
-        public int? SourceNodeGroupId { get; set; }
-        public int? TargetNodeGroupId { get; set; }
+        public int SourceNodeGroupId { get; set; }
+        public int TargetNodeGroupId { get; set; }
 
         public Project Project { get; set; }
         public NodeGroup SourceNodeGroup { get; set; }
--- SymmetricDS.Admin.Data/Server/ServerDbContext.cs
+++ SymmetricDS.Admin.Data/Server/ServerDbContext.cs
@@ -23,13 +23,13 @@ namespace SymmetricDS.Admin.Server
         public virtual DbSet<Trigger> Trigger { get; set; }
         public virtual DbSet<TriggerRouter> TriggerRouter { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=Sym;Username=postgres;port=5432");
-        //    }
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=Sym;Username=postgres;port=5432");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -129,13 +129,11 @@ namespace SymmetricDS.Admin.Server
                 entity.HasOne(d => d.SourceNodeGroup)
                     .WithMany(p => p.RouterSourceNodeGroup)
                     .HasForeignKey(d => d.SourceNodeGroupId)
-                    .OnDelete(DeleteBehavior.SetNull)
                     .HasConstraintName("FK__Router__SourceNo__5812160E");
 
                 entity.HasOne(d => d.TargetNodeGroup)
                     .WithMany(p => p.RouterTargetNodeGroup)
                     .HasForeignKey(d => d.TargetNodeGroupId)
-                    .OnDelete(DeleteBehavior.SetNull)
                     .HasConstraintName("FK__Router__TargetNo__59063A47");
             });
 
@@ -145,7 +143,9 @@ namespace SymmetricDS.Admin.Server
                     .HasName("Trigger_ChannelId_TriggerId_key")
                     .IsUnique();
 
-                entity.Property(e => e.SourceTableName).HasMaxLength(255);
+                entity.Property(e => e.SourceTableName)
+                    .IsRequired()
+                    .HasMaxLength(255);
 
                 entity.Property(e => e.TriggerId)
                     .IsRequired()
